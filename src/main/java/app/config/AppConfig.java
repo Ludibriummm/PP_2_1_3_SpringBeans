8a089b6b98beadb3c9277f3122afa60cb8dd6b62
Marina Panina <paninamar235@gmail.com>
2022-12-24 02:26:17 +1000

    Complete task

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,26 @@
 package app.config;
 
+import app.model.Animal;
+import app.model.AnimalsCage;
+import app.model.Cat;
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name = "dog")
+    public Animal getDog(){
+    return new Dog();
+    }
+    @Bean(name = "cat")
+    public Animal getCat(){
+    return new Cat();
+    }
+    @Bean(name = "animalsCage")
+    public AnimalsCage getAnimalsCage(){
+    return new AnimalsCage();
+    }
 }
